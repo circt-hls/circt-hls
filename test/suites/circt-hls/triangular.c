0dc97bfd01dede6253e7ff113eb37178b5db9dcb
Morten Borup Petersen <morten_bp@live.dk>
2021-11-24 17:03:14 +0000

    Remove examples directory

--- test/suites/circt-hls/triangular.c
+++ test/suites/circt-hls/triangular.c
@@ -1,15 +1,12 @@
-
 #define N 8
 int triangular(int i) {
   int acc = 0;
   int arr[N];
-  for(int j = 0; j < i; ++j)
+  for (int j = 0; j < i; ++j)
     arr[j] = j;
 
-  for(int j = 0; j < i; ++j)
+  for (int j = 0; j < i; ++j)
     acc += arr[j];
 
   return acc;
 }
-
-
