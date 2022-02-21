1cda0d40a1d2cc1acb0c559f3c436de7b8413dd7
Morten Borup Petersen <morten_bp@live.dk>
2022-02-21 13:58:49 +0100

    More LLVM sync updates

--- tools/hls-opt/hls-opt.cpp
+++ tools/hls-opt/hls-opt.cpp
@@ -17,6 +17,7 @@
 #include "circt/Support/LoweringOptions.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SCF/SCF.h"
@@ -33,6 +34,7 @@ int main(int argc, char **argv) {
   registry.insert<mlir::LLVM::LLVMDialect>();
   registry.insert<mlir::memref::MemRefDialect>();
   registry.insert<mlir::StandardOpsDialect>();
+  registry.insert<mlir::cf::ControlFlowDialect>();
   registry.insert<mlir::arith::ArithmeticDialect>();
   registry.insert<mlir::scf::SCFDialect>();
 
